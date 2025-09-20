907bc6e4fc81eaf50470f8f319d7255f2a30d9eb
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-09-20 17:25:58 +0600

    daisy, tailwind, and react router setup

---
+++ src/layout/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../shared/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../shared/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet></Outlet>
+            <Footer />
+
+        </div>
+    );
+};
+
+export default Main;
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            
+            <img src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2021/12/2x_logo.png" alt="" />
+            <div>
+
+            </div>
         </div>
     );
 };
