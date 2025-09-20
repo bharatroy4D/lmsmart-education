ec51a8141964c9e35e18d9eeba4ba0c3016ad8aa
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-09-20 17:34:08 +0600

    navbar of img and link setup

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
-            <img src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2021/12/2x_logo.png" alt="" />
-            <div>
-
+            <div className='flex justify-between items-center max-w-11/12 mx-auto'>
+                <img className='w-48' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2021/12/2x_logo.png" alt="" />
+                <div className='flex gap-4 text-lg font-medium'>
+                    <Link to={"/"}>Home</Link>
+                    <Link to={"/course"}>Course</Link>
+                    <Link to={"/blog"}>Blog</Link>
+                    <Link to={"/pages"}>Pages</Link>
+                </div>
             </div>
         </div>
     );
