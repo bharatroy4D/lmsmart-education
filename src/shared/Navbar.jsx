0519bc53789bb7dd7bc59d70c12914d5aa4fbf85
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-09-20 20:28:58 +0600

    Navbar into react icon added

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { CiSearch } from 'react-icons/ci';
+import { MdOutlineShoppingCart } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <div className='flex justify-between items-center max-w-11/12 mx-auto'>
-                <img className='w-48' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2021/12/2x_logo.png" alt="" />
-                <div className='flex gap-4 text-lg font-medium'>
-                    <Link to={"/"}>Home</Link>
-                    <Link to={"/course"}>Course</Link>
-                    <Link to={"/blog"}>Blog</Link>
-                    <Link to={"/pages"}>Pages</Link>
+        <div className='bg-blue-200'>
+            <div className='flex justify-between items-center py-3 border-gray-600 max-w-11/12 mx-auto'>
+                <img className='w-36' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2021/12/2x_logo.png" alt="" />
+
+                <div className='flex items-center'>
+                    <div className='flex gap-7 text-gray-700 font-medium'>
+                        <Link to={"/"}>Home</Link>
+                        <Link to={"/course"}>Course</Link>
+                        <Link to={"/blog"}>Blog</Link>
+                        <Link to={"/pages"}>Pages</Link>
+                    </div>
+                    {/* search bar */}
+                    <div className='flex border border-gray-400 text-gray-700 py-1 px-3 ml-10'>
+                        <input type="text" name="" id="" placeholder='Search' />
+                        <CiSearch className='text-xl' />
+                    </div>
+                    <MdOutlineShoppingCart  className='tex-2xl text-gray-600 ml-5'/>
                 </div>
             </div>
         </div>
