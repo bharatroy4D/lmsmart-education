50b83d3ad8bd3ca9ce41a66301dc4d6e992dce05
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-09-22 15:36:00 +0600

    navbar icon and menu item again size change

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     return (
         <div className='bg-blue-200'>
             <div className='flex justify-between items-center py-2 lg:py-3  max-w-11/12 mx-auto'>
-                <img className='w-28 lg:w-36' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2021/12/2x_logo.png" alt="" />
+                <img className='w-32 lg:w-36' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2021/12/2x_logo.png" alt="" />
 
                 <div>
                     <div className='flex items-center'>
@@ -25,7 +25,7 @@ const Navbar = () => {
                             <input className='outline-none' type="text" name="" id="" placeholder='Search' />
                             <CiSearch className='text-xl' />
                         </div>
-                        <MdOutlineShoppingCart className='tex-4xl text-gray-600 mx-7' />
+                        <MdOutlineShoppingCart className='text-3xl text-gray-600 mx-7' />
                         <div className='hidden lg:block text-gray-600 border-l border-gray-700 pl-10'>
                             <Link to={"/signin"}>Login</Link>/
                             <Link to={"/signup"}>Signup</Link>
@@ -33,14 +33,14 @@ const Navbar = () => {
                         {/* mobile device */}
                         <div className='md:hidden'>
                             <RxHamburgerMenu onClick={() => setOpen(!open)}
-                                className='text-xl text-gray-600' />
+                                className='text-2xl text-gray-600' />
                         </div>
                         <div className={`fixed md:hidden top-0 right-0 p-3 w-36 bg-white shadow-md rounded-2xl transform transition duration-300
                             ${open ? ' translate-x-0' : 'translate-x-full'}
                             `} >
                             <div className='flex justify-between items-center w-full border-b border-gray-300 pb-2 mb-5'>
-                                <h1 className='text-xl text-gray-600 font-medium'>Menu</h1>
-                                <IoMdClose onClick={()=>setOpen(!open)} className='text-2xl text-black' />
+                                <h1 className='text-lg text-gray-600 font-medium'>Menu</h1>
+                                <IoMdClose onClick={()=>setOpen(!open)} className='text-xl text-black' />
 
                             </div>
                             {open && (
