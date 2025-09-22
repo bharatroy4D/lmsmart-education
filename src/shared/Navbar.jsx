812e05a02a1b4b044bf6f4eed4ef308aaaaa4188
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-09-22 14:39:09 +0600

    Responsive menu items added and click to show

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CiSearch } from 'react-icons/ci';
 import { MdOutlineShoppingCart } from 'react-icons/md';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     return (
         <div className='bg-blue-200'>
-            <div className='flex justify-between items-center py-3  max-w-11/12 mx-auto'>
+            <div className='flex justify-between items-center py-2 lg:py-3  max-w-11/12 mx-auto'>
                 <img className='w-28 lg:w-36' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2021/12/2x_logo.png" alt="" />
 
                 <div>
@@ -30,8 +31,20 @@ const Navbar = () => {
                         </div>
                         {/* mobile device */}
                         <div className='md:hidden'>
-                            <RxHamburgerMenu className='text-xl text-gray-600' />
+                            <RxHamburgerMenu onClick={() => setOpen(!open)}
+                                className='text-xl text-gray-600' />
+                        </div>
+                        <div className='absolute md:hidden top-12 right-0 py-2 px-10 bg-white shadow-md rounded-2xl transform transition duration-1000'>
+                            {open && (
+                                <div className='flex flex-col gap-5 text-gray-700 '>
+                                    <Link to={"/"}>Home</Link>
+                                    <Link to={"/course"}>Course</Link>
+                                    <Link to={"/blog"}>Blog</Link>
+                                    <Link to={"/pages"}>Pages</Link>
+                                </div>
+                            )
 
+                            }
                         </div>
                     </div>
 
