e212a9a718720df6b9a186efd3e439cb0ebb5715
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-09-23 17:26:07 +0600

    banner images setup

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='w-11/12 mx-auto'>
-            <div>
-                <h3>Developed By To Teachers</h3>
-                <h1>Experience a learning platform that take you next level</h1>
-                <h2>World-class training and development programs developed by top teachers</h2>
-                <button className='btn btn-primary'>button</button>
+        <div className='bg-base-200'>
+            <div className='flex justify-between items-center w-11/12 mx-auto'>
+                {/* banner context text */}
+                <div>
+                    <h3>Developed By To Teachers</h3>
+                    <h1>Experience a learning platform that take you next level</h1>
+                    <h2>World-class training and development programs developed by top teachers</h2>
+                    <button className='btn btn-primary'>button</button>
+                </div>
+                {/* banner content image */}
+                <div className='flex '>
+                    <img className='lg:h-72' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2022/02/HR0835.jpg" alt="" />
+                    <img src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2022/02/HTR084.jpg" alt="" />
+                </div>
             </div>
+
         </div>
     );
 };
