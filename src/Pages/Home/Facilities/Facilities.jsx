81a61a2d64220a056b532a3106ecfa082c593b2f
bharatroy4D <bharatroy1005@gmail.com>
2025-09-24 23:13:27 +0600

    Facilities component design complite

--- src/Pages/Home/Facilities/Facilities.jsx
+++ src/Pages/Home/Facilities/Facilities.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Facilities = () => {
     return (
         <div className='bg-blue-950'>
-            <div className='container lg:flex gap-8 px-4 lg:px-10 mx-auto'>
+            <div className='container lg:flex items-center justify-between gap-8 px-4 lg:px-10 mx-auto'>
                 {/* card 1 */}
                 <div className='flex items-center gap-6 py-7 text-white'>
                     <img className='w-14' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2022/01/III98456.png" alt="" />
@@ -16,16 +16,16 @@ const Facilities = () => {
                 <div className='flex items-center gap-6 py-7 text-white'>
                     <img className='w-14' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2022/01/icon-2.png" alt="" />
                     <div className='flex flex-col gap-2'>
-                        <h1 className='text-2xl font-medium'>Over 155,000</h1>
-                        <h3 className=''>Video courses on career skills</h3>
+                        <h1 className='text-2xl font-medium'>Choose from</h1>
+                        <h3 className=''>Top industry instructors</h3>
                     </div>
                 </div>
-                {/* card 1 */}
+                {/* card 3 */}
                 <div className='flex items-center gap-6 py-7 text-white'>
-                    <img className='w-16' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2022/01/III98456.png" alt="" />
+                    <img className='w-14' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2022/01/icon-3.png" alt="" />
                     <div className='flex flex-col gap-2'>
-                        <h1 className='text-3xl font-medium'>Over 155,000</h1>
-                        <h3 className=''>Video courses on career skills</h3>
+                        <h1 className='text-2xl font-medium'>Lifetime access</h1>
+                        <h3 className=''>On mobile and desktop</h3>
                     </div>
                 </div>
             </div>
