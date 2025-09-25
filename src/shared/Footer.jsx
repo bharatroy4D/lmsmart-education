36d3183381634ef9bfb902cb6b33d84fe60a7e07
bharatroy4D <bharatroy1005@gmail.com>
2025-09-25 17:17:54 +0600

    Footer component is desing done

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { AiFillInstagram } from 'react-icons/ai';
+import { FaTwitter } from 'react-icons/fa';
+import { MdOutlineFacebook } from 'react-icons/md';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='bg-blue-950'>
+            <div className='container px-4 py-7 lg:py-20 lg:px-10 mx-auto '>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 items-center'>
+                    {/* card 1 */}
+                    <div className='space-y-3 text-white'>
+                        <img className='w-50 ' src="https://thepixelcurve.com/wp/lmsmart/lp/wp-content/uploads/sites/2/2021/12/2x_logo-white.png" alt="" />
+                        <p className='text-xl '>We support programs that create advancement for people</p>
+                        {/* Social icon */}
+                        <div className='flex items-center gap-5'>
+                            <div className='w-fit border-2 border-gray-400 rounded-full'>
+                                <MdOutlineFacebook className='text-3xl p-1  ' />
+                            </div>
+                            <div className='w-fit border-2 border-gray-400 rounded-full'>
+                                <FaTwitter className='text-3xl p-1' />
+                            </div>
+                            <div className='w-fit border-2 border-gray-400 rounded-full'>
+                                <AiFillInstagram className='text-3xl p-1' />
+                            </div>
+
+                        </div>
+                    </div>
+                    {/* card 2 */}
+                    <div className='text-white'>
+                        <h1 className='text-2xl font-semibold'>useful links</h1>
+                        <div className='flex flex-col gap-3 font-semibold mt-5'>
+                            <p>About Us</p>
+                            <p>Privacy Policy</p>
+                            <p>Terms & condition</p>
+                            <p>Student spotlight</p>
+                        </div>
+                    </div>
+                    {/* card 3 */}
+                    <div className='text-white'>
+                        <h1 className='text-2xl font-semibold'>Learning</h1>
+                        <div className='flex flex-col gap-3 font-semibold mt-5'>
+                            <p>Business Strategy</p>
+                            <p>Become A Teacher</p>
+                            <p>Project Management</p>
+                            <p>Membership</p>
+                        </div>
+                    </div>
+                    {/* card 4 */}
+                    <div className='text-white'>
+                        <h1 className='text-2xl font-semibold'>Contact Us</h1>
+                        <div className='flex flex-col gap-3 font-semibold mt-5'>
+                            <p>+8801714414775</p>
+                            <p>information@example.com</p>
+                            <p>Biral Dinajpur, Dhaka </p>
+                            <p>Bangladesh Asian Country</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
