314064990e26aab872c1f18794127cb2ee1af67e
bharatroy4D <bharatroy1005@gmail.com>
2025-09-25 13:11:04 +0600

    Course card design is complite

--- src/Pages/Home/Tutorial/Tutorial.jsx
+++ src/Pages/Home/Tutorial/Tutorial.jsx
@@ -15,9 +15,19 @@ const Tutorial = () => {
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 py-8 lg:py-15'>
                     {
                         course.map(card => (
-                            <div key={card.id} >
-                                <img className='rounded h-40 w-72' src={card.image} alt="" />
-                                
+                            <div key={card.id} className='rounded shadow-lg' >
+                                <img className='rounded h-40 w-72 ' src={card.image} alt="" />
+                                <div className='space-y-5 p-5'>
+                                    <div className='flex items-center justify-between mt-4'>
+                                        <h1 className='bg-gray-100 py-0.5 px-2 rounded text-gray-500 font-medium'>{card.category}</h1>
+                                        <h2 className='font-medium'>{card.price}</h2>
+                                    </div>
+                                    <h1 className='text-xl font-medium text-gray-800'>{card.title}</h1>
+                                    <div className='flex items-center gap-3'>
+                                        <img className='w-8 rounded-full' src={card.author} alt="" />
+                                        <h1 className='text-gray-400 font-semibold'>{card.author_name}</h1>
+                                    </div>
+                                </div>
                             </div>
                         ))
                     }
