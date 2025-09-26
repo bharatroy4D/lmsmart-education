31d30ce4ef116cc5cabaa3df013587f9b0ef6e53
bharatroy4D <bharatroy1005@gmail.com>
2025-09-26 23:31:14 +0600

    AuthContext is create

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,3 +1,38 @@
-import { createContext } from "react"
+import { Children, createContext, useState } from "react"
+import { login, register } from "../api/authApi"
 
 const AuthContext = createContext()
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+    // login
+    const Login = async (data) => {
+        try {
+            const res = await login(data)
+            if (res?.token) {
+                localStorage.setItem("token", res.token)
+                setUser(res?.user || null)
+                return res;
+            }
+        } catch (err) {
+            console.error("login is failed", err);
+            throw err
+        }
+    }
+    // register
+    const Register = async (data) => {
+        try {
+            const res = await register(data)
+            setUser(res?.data || null)
+            return res;
+        } catch (err) {
+            console.error("register is failed", err)
+            throw err
+        }
+    }
+    return (
+        <AuthContext.Provider value={{ Login, user }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
