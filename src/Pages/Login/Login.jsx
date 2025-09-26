d11362fc694289a8d17bbfd358bc0702b8da8f14
bharatroy4D <bharatroy1005@gmail.com>
2025-09-26 23:56:47 +0600

    login page is update

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,20 +1,7 @@
 import React, { useState } from "react";
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Login Data:", formData);
-    // You can integrate API here
-  };
+ 
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
