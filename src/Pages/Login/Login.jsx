963bf70422d3fe78b341aeea698e1dc4b3f7e52e
bharatroy4D <bharatroy1005@gmail.com>
2025-09-28 23:38:17 +0600

    Login page into Loading section added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -75,7 +75,7 @@ const Login = () => {
             type="submit"
             className="w-full bg-blue-500 text-white py-2 rounded-lg font-semibold hover:bg-blue-600 transition"
           >
-            Login
+            {loading? "Login..." : "Login"}
           </button>
         </form>
 
